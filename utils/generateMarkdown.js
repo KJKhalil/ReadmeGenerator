7a8435e4e7ecb61a3dad842227868b39a1b633e3
TacTac <110001003+KJKhalil@users.noreply.github.com>
2022-10-19 20:44:24 -0400

    Made the renderLicenseSection function as well as the generateMarkdown function to create the layout of the readme.

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  if (!license){
+  if (!license) {
     return '';
   }
   else {
@@ -24,7 +24,7 @@ function renderLicenseLink(license) {
   if (license === 'GNU') {
     return 'https://www.gnu.org/licenses/gpl-3.0'
   }
-  
+
   if (license === 'ISC') {
     return 'https://opensource.org/licenses/ISC'
   }
@@ -32,12 +32,52 @@ function renderLicenseLink(license) {
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (!license) {
+    return '';
+  }
+  else {
+    return '## License(s)'
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
+  ${renderLicenseBadge(data.license)}
+
+  ## Table Of Contents
+  * [Description](#description)
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Contribution](#contribution)
+  * [Test](#test)
+  * [License(s)](#license)
+  * [Question(s)](#questions)
+
+  ## Description
+  ${data.description}
+
+  ## Installation
+  ${data.installation}
+
+  ## Usage
+  ${data.usage}
+
+  ## Contribution
+  ${data.contribution}
+
+  ## Test
+  ${data.test}
+
+  ${renderLicenseSection(data.license)}
+  ${renderLicenseLink(data.license)}
+
+  ## Question(s)
+  If you have any questions about this project you can reach me at:
+  GitHub: https://github.com/${data.github}
+  Email: ${data.email}
 `;
 }
 
